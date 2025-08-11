692a997c5065ef5168917ee73e67330da9e3bf76
3 <11862956+3-ark@users.noreply.github.com>
2025-08-11 17:25:39 +0800

    Update textarea.tsx

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -37,6 +37,7 @@ function Textarea({
           "focus-visible:border-ring focus-visible:ring-ring/50",
           "text-sm md:text-sm transition-[color,box-shadow] outline-none focus-visible:ring-[3px]",
           "disabled:cursor-not-allowed disabled:opacity-50",
+          "bg-[var(--input-background)]",
           "thin-scrollbar",
           className
         )}
@@ -49,7 +50,7 @@ function Textarea({
     <textarea
       data-slot="textarea-default"
       className={cn(
-        "border-[var(--text)]/20 placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border px-3 py-2 text-sm shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "border-[var(--text)]/20 bg-[var(--input-background)] placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border px-3 py-2 text-sm shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         className
       )}
       {...props}
